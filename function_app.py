d8f7aa2399d84ed5f8ec12220df99ec987302549
MisterPellino <marco.pellino@curevac.com>
2024-08-22 14:50:12 +0200

    Import blueprint for ff.

--- function_app.py
+++ function_app.py
@@ -1,6 +1,10 @@
 import azure.functions as func
-from blueprints import http_parse_to_csv_fb
+from blueprints import (
+    http_parse_to_csv_fb,
+    http_parse_to_csv_ff
+)
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 app.register_functions(http_parse_to_csv_fb.bp)
+app.register_functions(http_parse_to_csv_ff.bp)
